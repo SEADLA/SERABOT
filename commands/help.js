bc7da12aa21e9d44f6576d18834eb66084a15a8e
SEADLA <71223166+SEADLA@users.noreply.github.com>
2020-10-20 21:06:11 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ module.exports.run = async (args, message, client) => {
     const help = new Discord.MessageEmbed() 
 	.setColor('RANDOM')
 	.setTitle('╚»★«╝ HELP COMMAND ╚»★«╝')
-	.setDescription("Prefix: ``sr!``\nCommand List:\n\n:man_police_officer_tone2: **Moderator Command**\n```say, clear, eval, kick, mute, unmute, warn, ban```\n:joy: **Fun Command**\n```meme, howgay, lucky, joke, advice, waifu```\n<a:Blob_megaparty:739515201015119972> **Utility Command**\n```ping, avatar, botinfo, math, youtube, serverinfo, userinfo, covid, weather, translate```\n:bird: **Animals**\n```bird, cat, cow, dog, panda, sheep```\n:pick: **Minecraft Command**\n```skin, mcserver```\n:moneybag: **Economy Command**\n```bal, daily, work```\n\n\n**Note:** ```Change Goodbye Channel : '𝙶𝙾𝙾𝙳𝙱𝚈𝙴' & Change Welcome Channel : '𝚆𝙴𝙻𝙲𝙾𝙼𝙴'```")
+	.setDescription("Prefix: ``sr!``\nCommand List:\n\n:man_police_officer_tone2: **Moderator Command**\n```say, clear, eval, kick, mute, unmute, warn, ban```\n:joy: **Fun Command**\n```meme, howgay, lucky, joke, advice, waifu```\n<:minecraft:764643150538080298> **Utility Command**\n```ping, avatar, botinfo, math, youtube, serverinfo, userinfo, covid, weather, translate```\n:bird: **Animals**\n```bird, cat, cow, dog, panda, sheep```\n:pick: **Minecraft Command**\n```skin, mcserver```\n:moneybag: **Economy Command**\n```bal, daily, work```\n\n\n**Note:** ```Change Goodbye Channel : '𝙶𝙾𝙾𝙳𝙱𝚈𝙴' & Change Welcome Channel : '𝚆𝙴𝙻𝙲𝙾𝙼𝙴'```")
 	.setThumbnail('https://i.imgur.com/enKWPMY.png')
     .setImage('')
 	.setTimestamp()
