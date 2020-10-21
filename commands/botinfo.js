a937ae8640139e71d8b7fff1effbdc2d18c48269
SEADLA <71223166+SEADLA@users.noreply.github.com>
2020-10-21 20:47:56 +0700

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -3,7 +3,7 @@ module.exports.run = async (client, message, args) => {
         const info = new Discord.MessageEmbed()
         .setColor('BLUE')
         .setAuthor('╚»★«╝ BOT INFO ╚»★«╝')
-        .setDescription("SERA bot is developed and created by SEADLA, on 15 sept 2020.\n\n**Discord Server:** [Join](https://discord.com/invite/KMF7Dq2)\n**Saweria:** [Donate](https://saweria.co/SEADLA12)\n**Youtube:** [SUBSCRIBE](https://www.youtube.com/channel/UCZKB41uud9hSGhCOvXBAWyQ/videos)\n**Official Website:** [SERA Website](https://serabot.glitch.me/)")
+        .setDescription("SERA bot is developed and created by SEADLA, on 15 sept 2020.\n\n**Discord Server:** [Join](https://discord.gg/5kpQR5Y)\n**Saweria:** [Donate](https://saweria.co/SEADLA12)\n**Youtube:** [SUBSCRIBE](https://www.youtube.com/channel/UCZKB41uud9hSGhCOvXBAWyQ/videos)\n**Official Website:** [SERA Website](https://serabot.glitch.me/)")
         .setThumbnail('https://i.imgur.com/enKWPMY.png')
         .addField('Official server :', 'SERA Bot', true)
         .setImage('')
