b60ce3821413e5664432e875b968a899ee4a3243
SEADLA <71223166+SEADLA@users.noreply.github.com>
2020-10-22 11:41:51 +0700

    Create clyde.js

---
+++ commands/clyde.js
@@ -0,0 +1,19 @@
+const fetch = require("node-fetch"); 
+const { MessageEmbed } = require("discord.js"); 
+module.exports = { 
+  name: "clyde", 
+  description: "Let clyde say something", 
+  category: "fun", 
+  async execute(bot, message, args) 
+  { const text = args.join(" "); 
+  if (!text) return message.reply("Please provide text"); 
+  const data = await fetch( `https://nekobot.xyz/api/imagegen?type=clyde&text=${text}` ).then((res) => res.json()); 
+  const embed = new MessageEmbed() 
+  .setTitle("Clyde") 
+  .setImage(data.message)
+  .setFooter(message.author.username) 
+  .setColor("BLUE") 
+  .setDescription( `[Click here if the image failed to load.](${data.message})` ); 
+  message.channel.send(embed); 
+    }, 
+  };
